96481076e2e89a056c56d7b8c0208f9bcb11b9a7
Vimal Buckley <vimalbuckley@gmail.com>
2025-11-01 13:55:53 -0400

    perfect code that works definetly
    it works (for sure)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,27 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Drive;
 
 public class RobotContainer {
+  private final Drive drive = new Drive();
+  private final XboxController controller = new XboxController(0);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    Command arcadeDrive = drive.runWithJoystick(
+      () -> MathUtil.applyDeadband(controller.getLeftX(), 0.1),
+      () -> MathUtil.applyDeadband(controller.getRightY(), 0.1)
+    );
+    drive.setDefaultCommand(arcadeDrive);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
